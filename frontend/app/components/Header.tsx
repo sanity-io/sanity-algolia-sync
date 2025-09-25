05d7d81900225b2190dac84df1ddde1ac532a904
irina <irina@sanity.io>
2025-09-25 16:50:31 -0400

    update links

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -31,7 +31,7 @@ export default async function Header() {
               <li className="sm:before:w-[1px] sm:before:bg-gray-200 before:block flex sm:gap-4 md:gap-6">
                 <Link
                   className="rounded-full flex gap-4 items-center bg-black hover:bg-blue focus:bg-blue py-2 px-4 justify-center sm:py-3 sm:px-6 text-white transition-colors duration-200"
-                  href="https://github.com/sanity-io/Algolia-Sanity-Function-Example"
+                  href="https://github.com/sanity-io/sanity-algolia-sync"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
